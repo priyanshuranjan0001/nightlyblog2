c09174e25d7502ee9b226cf238c8f991c7b5c5f6
priyanshu ranjan <37440986+nightly-dev@users.noreply.github.com>
2020-06-07 23:43:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,39 +1,35 @@
 module.exports = {
-  url: 'https://dospolov.com',
+  url: 'https://nightlyblog.netlify.app',
   pathPrefix: '/',
-  title: 'Blog by Marat Dospolov',
+  title: 'Nightly Blog',
   keywords:
     'account surfer, trello bookmark, trello, productivity, javascript, Marat Dospolov, choosy windows, browser selector',
-  subtitle: 'Developer. Optimizer. Learner.',
-  copyright: '© 2020 | Kyiv, Ukraine',
-  disqusShortname: 'dospolov',
+  subtitle: 'The Best Blog To Learn Programming',
+  copyright: '© 2020 | Nightly Developer',
+  disqusShortname: 'Nightl_ydev',
   postsPerPage: 20,
   googleAnalyticsId: 'UA-6589522-7',
   menu: [
     {
       label: 'Blog',
       path: '/'
-    },
-    {
-      label: 'CV',
-      path: '/cv'
     }
   ],
   author: {
-    name: 'Marat Dospolov',
-    photo: '/photo.jpg',
-    bio: 'Developer. Optimizer. Learner.',
+    name: 'Nightly Developer',
+    photo: '/logo.png',
+    bio: 'The Best Blog To Learn Programming',
     contacts: {
       // don't remove fields, just make them empty string ''
       // https://github.com/gatsbyjs/gatsby/issues/2392
-      github: 'dospolov',
-      twitter: 'marat_dospolov',
-      linkedin: 'dospolov',
-      telegram: 'dospolov',
-      instagram: 'dospolov',
-      facebook: 'dospolov',
+      github: 'nightly_dev',
+      twitter: 'nightly_dev',
+      linkedin: '',
+      telegram: '',
+      instagram: 'nightly_dev',
+      facebook: '',
       email: '',
-      rss: '/rss.xml'
+      rss: ''
     }
   }
 }
